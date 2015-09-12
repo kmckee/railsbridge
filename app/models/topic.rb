9d620fc57c0d91b0c88c4f38711a2239a80b18e6
Kyle McKee <mckee.kyle@gmail.com>
2015-09-12 15:34:01 -0400

    Added stuff.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,4 @@
 class Topic < ActiveRecord::Base
     has_many :votes, dependent: :destroy
+    belongs_to :author, class_name: User, foreign_key: :user_id
 end
---
+++ db/migrate/20150912184109_add_author_to_topic.rb
@@ -0,0 +1,5 @@
+class AddAuthorToTopic < ActiveRecord::Migration
+  def change
+    add_column :topics, :user_id, :integer
+  end
+end
